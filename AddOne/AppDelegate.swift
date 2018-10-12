2bd8ca54c0fd99d14c5393e29357b100ab962103
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-12 17:56:45 -0400

    initial setup

--- AddOne/AppDelegate.swift
+++ AddOne/AppDelegate.swift
@@ -4,10 +4,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var mainViewController: MainViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        mainViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        
+        window!.rootViewController = mainViewController
+        window!.makeKeyAndVisible()
+
         return true
     }
 
