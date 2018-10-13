5d02e70907a43cd9ac18516b9b279301aa4e0dd6
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-10-13 09:05:04 -0400

    UI is finished

--- AddOne/AppDelegate.swift
+++ AddOne/AppDelegate.swift
@@ -10,14 +10,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         mainViewController = MainViewController(nibName: "MainViewController", bundle: nil)
+        
         let frame = UIScreen.main.bounds
         window = UIWindow(frame: frame)
-        
         window!.rootViewController = mainViewController
         window!.makeKeyAndVisible()
 
         return true
     }
-
 }
 
